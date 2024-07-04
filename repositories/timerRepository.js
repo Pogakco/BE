0d2ce46cbae42888b3f30543efd6ffe4bd7ba186
Eunchae Kim <kec9706@gmail.com>
2024-07-05 01:57:21 +0900

    feat(room, timer): 타이머 구현에 필요한 repository 메서드 추가

---
+++ repositories/timerRepository.js
@@ -0,0 +1,39 @@
+const timerRepository = {
+  async updateTimerToStart({ connection, roomId }) {
+    const SQL = `
+      UPDATE timers
+      SET 
+        started_at = NOW(),
+        is_running = true
+      WHERE room_id = ?
+    `;
+
+    await connection.query(SQL, [roomId]);
+  },
+
+  async updateTimerToFinish({ connection, roomId }) {
+    const SQL = `
+      UPDATE timers
+      SET
+        started_at = NULL,
+        is_running = false,
+        current_cycles = 0
+      WHERE room_id = ?
+    `;
+
+    await connection.query(SQL, [roomId]);
+  },
+  
+  async updateTimerCurrentCycles({ connection, roomId, currentCycles }) {
+    const SQL = `
+      UPDATE timers
+      SET
+        current_cycles = ?
+      WHERE room_id = ?
+    `;
+
+    await connection.query(SQL, [currentCycles, roomId]);
+  },
+}
+
+export default timerRepository
