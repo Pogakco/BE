e6ae6a026ffb639decfb23ad3251a88f43735d97
Hanbi <80617446+hanbirang@users.noreply.github.com>
2024-07-04 16:51:04 +0900

    feat(roomList): 방 목록 API 추가  (#3)

---
+++ services/roomListService.js
@@ -0,0 +1,25 @@
+import roomListRepository from "../repositories/roomListRepository.js";
+
+const roomListService = {
+  async getRooms({ connection, page, limit, isRunning }) {
+    const offset = (page - 1) * limit;
+    const { data, totalElements } = await roomListRepository.findAllRooms({
+      connection,
+      offset,
+      limit,
+      isRunning,
+    });
+
+    return {
+      data,
+      pagination: {
+        currentPage: parseInt(page),
+        totalPages: Math.ceil(totalElements / limit),
+        totalElements,
+        limit: parseInt(limit),
+      },
+    };
+  },
+};
+
+export default roomListService;
