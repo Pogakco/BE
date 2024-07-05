a6aa4993bc5cd6590747dc0261f45160bae4f8a7
Eunchae Kim <kec9706@gmail.com>
2024-07-06 00:18:44 +0900

    feat(user-validator): password signup chain을 따로 분리해서 정규표현식 유효성 검사 추가

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -12,9 +12,24 @@ const createEmailChain = () => {
 
 const createNicknameChain = () => {
   return body("nickname").notEmpty().withMessage("닉네임을 입력해 주세요.");
+const createSignupPasswordChain = () => {
+  // 알파벳, 숫자, 특수문자가 1개씩 포함되는 8-20자 내의 값
+  const alphabets = "A-Za-z";
+  const numbers = "\\d";
+  const specialChars = "@$!%*#?&";
+  const regex = new RegExp(
+    `^(?=.*[${alphabets}])(?=.*[${numbers}])(?=.*[${specialChars}])[${alphabets}${numbers}${specialChars}]{8,20}$`
+  );
+
+  return body("password")
+    .notEmpty()
+    .withMessage("비밀번호를 입력해 주세요.")
+    .bail()
+    .matches(regex)
+    .withMessage("비밀번호를 형식에 맞게 입력해 주세요.");
 };
 
-const createPasswordChain = () => {
+const createLoginPasswordChain = () => {
   return body("password").notEmpty().withMessage("비밀번호를 입력해 주세요.");
 };
 
@@ -23,12 +38,12 @@ const userValidator = {
     return createValidator(
       createEmailChain,
       createNicknameChain,
-      createPasswordChain
+      createSignupPasswordChain
     );
   },
 
   getLoginValidator() {
-    return createValidator(createEmailChain, createPasswordChain);
+    return createValidator(createEmailChain, createLoginPasswordChain);
   },
 };
 
