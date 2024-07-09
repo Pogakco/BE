bad65ba8d8c6bea57b7504842e4cd73b1cc68f87
Hanbi <adray0625@naver.com>
2024-07-09 17:13:43 +0900

    feat(room): 방 삭제하기 api 구현

--- services/roomService.js
+++ services/roomService.js
@@ -113,6 +113,10 @@ const roomService = {
   async leaveRoom({ connection, userId, roomId }) {
     await userRoomRepository.removeUserFromRoom({ connection, userId, roomId });
   },
+
+  async deleteRoom({connection, roomId}) {
+    await roomRepository.delteRoom({ connection, roomId });
+  }
 };
 
 export default roomService;
