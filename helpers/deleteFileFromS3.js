4dcf8cf89f8aa9af80ca9a11cac9d6f701a32f65
Eunchae Kim <kec9706@gmail.com>
2024-07-10 17:58:39 +0900

    feat(util): 함수 재사용을 위해 trimSlash 함수를 trim 함수로 변경

--- helpers/deleteFileFromS3.js
+++ helpers/deleteFileFromS3.js
@@ -1,12 +1,12 @@
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "../config/aws-config.js";
-import { trimStartSlash } from "../utils/trimSlash.js";
+import { trimStart } from "../utils/trim.js";
 
 const deleteFileFromS3 = async ({ pathname }) => {
   try {
     const deleteObjectCommand = new DeleteObjectCommand({
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: trimStartSlash(pathname), // Key는 '/'로 시작하지 않아서, 혹여나 삭제되지 않는 경우를 방지
+      Key: trimStart(pathname, "/"), // Key는 '/'로 시작하지 않아서, 혹여나 삭제되지 않는 경우를 방지
     });
     const response = await s3Client.send(deleteObjectCommand);
 
--- helpers/uploadFileToS3.js
+++ helpers/uploadFileToS3.js
@@ -3,14 +3,12 @@ import { randomUUID } from "crypto";
 import { extname } from "path";
 import { s3Client } from "../config/aws-config.js";
 import { AWS_S3_DIRECTORY } from "../constants.js";
-import { trimSlash, trimStartSlash } from "../utils/trimSlash.js";
+import { trim, trimStart } from "../utils/trim.js";
 
 const uploadFileToS3 = async ({ file, directory }) => {
   try {
-    // 저장 경로가 '/foo//file.jpg'와 같이 되는 것을 방지하고, directory가 없거나 공백일 경우 디폴트 경로로 처리
-    directory = !!directory
-      ? trimSlash(directory)
-      : AWS_S3_DIRECTORY.DEFAULT;
+    // Key값이 '/foo//file.jpg'와 같이 되는 것을 방지하고, directory가 없거나 공백일 경우 디폴트 경로로 처리
+    directory = !!directory ? trim(directory, "/") : AWS_S3_DIRECTORY.DEFAULT;
 
     const extensionName = extname(file.originalname); // 확장자명('.' 포함)
     const fileName = randomUUID() + extensionName;
@@ -19,7 +17,7 @@ const uploadFileToS3 = async ({ file, directory }) => {
 
     const putObjectCommand = new PutObjectCommand({
       Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Key: trimStartSlash(filePathname), // S3에 "/" 디렉터리가 생기는걸 방지
+      Key: trimStart(filePathname, "/"), // S3에 "/" 디렉터리가 생기는걸 방지
       Body: file.buffer,
     });
 
