5e0cc2fd9e712920b6789d6c19a334a4ee80f79a
Hanbi <adray0625@naver.com>
2024-07-15 17:45:25 +0900

    feat(feedbackValidator): 피드백 글자수 유효성 검사 추가

--- validators/feedbackValidator.js
+++ validators/feedbackValidator.js
@@ -2,7 +2,12 @@ import { body } from "express-validator";
 import createValidator from "./helpers/createValidator.js";
 
 const createFeedbackChain = () => {
-  return body("contents").notEmpty().withMessage("내용을 입력해주세요.");
+  return body("contents")
+    .notEmpty()
+    .withMessage("내용을 입력해주세요.")
+    .bail()
+    .isLength({ max: 500 })
+    .withMessage("피드백 내용은 500자 이내여야 합니다.");
 };
 
 const feedbackValidator = {
