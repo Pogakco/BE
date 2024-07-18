5ebf400b50022d195bd777c9bac553597862c636
Eunchae Kim <123533586+lucaseunchae@users.noreply.github.com>
2024-07-18 20:02:37 +0900

    feat(timer-socket): 타이머 소켓에 연결 될 때 isRunning 상태를 동기화 하도록 변경 (#88)
    * refactor(timer-socket): 중복 사용되는 코드 제거
    
    * feat(timer-socket): 소켓 연결 될 때 isRunning 상태 동기화 하도록 변경
    
    * docs: 타이머 onConnection 핸들러에 기능별 설명 주석 추가

--- socket/room-detail/handlers/onRoomDetailConnection.js
+++ socket/room-detail/handlers/onRoomDetailConnection.js
@@ -6,6 +6,7 @@ import {
 import roomService from "../../../services/roomService.js";
 import getAllLinkedUserIdsFromNamespace from "../../helpers/getAllLinkedUserIdsFromNamespace.js";
 import getRoomIdFromNamespace from "../helpers/getRoomIdFromNamespace.js";
+import getRoomInfoSafety from "../helpers/getRoomInfoSafety.js";
 import registerRoomDetailEventsInterceptor from "../helpers/registerRoomDetailEventsInterceptor.js";
 import onDeleteRoom from "./onDeleteRoom.js";
 import onRoomDetailDisconnect from "./onRoomDetailDisconnect.js";
@@ -14,11 +15,11 @@ import onStartCycles from "./onStartCycles.js";
 const onConnection = async (socket) => {
   registerRoomDetailEventsInterceptor(socket);
 
-  const roomId = getRoomIdFromNamespace(socket.nsp);
-
+  const roomDetailNamespace = socket.nsp;
+  const roomId = getRoomIdFromNamespace(roomDetailNamespace);
   socket.join(roomId);
 
-  const roomDetailNamespace = socket.nsp;
+  // 모든 로그인한 유저 정보 동기화
   roomDetailNamespace
     .to(roomId)
     .emit(
@@ -26,6 +27,21 @@ const onConnection = async (socket) => {
       getAllLinkedUserIdsFromNamespace(roomDetailNamespace)
     );
 
+  // 방의 타이머 진행 상태 동기화
+  const { roomInfo, isErrorGetRoomInfo } = await getRoomInfoSafety({
+    socket,
+  });
+  if (isErrorGetRoomInfo) {
+    socket.emit(SOCKET_TIMER_EVENTS.ERROR, {
+      message: SOCKET_TIMER_ERRORS.INTERNAL_SERVER_ERROR,
+    });
+    return;
+  }
+  roomDetailNamespace
+    .to(roomId)
+    .emit(SOCKET_TIMER_EVENTS.SYNC_IS_RUNNING, roomInfo.isRunning);
+
+  // 참가자 정보 동기화
   try {
     const { users: allParticipants } =
       await roomService.getRoomUsersAndActiveCount({ roomId });
@@ -42,6 +58,7 @@ const onConnection = async (socket) => {
 
   console.log("A user connected to room:", roomId);
 
+  // 리스닝 이벤트 등록
   socket.on(SOCKET_TIMER_EVENTS.START_CYCLES, () => onStartCycles(socket));
   socket.on(SOCKET_TIMER_EVENTS.DELETE_ROOM, () => onDeleteRoom(socket));
   socket.on(SOCKET_DEFAULT_EVENTS.DISCONNECT, () =>
